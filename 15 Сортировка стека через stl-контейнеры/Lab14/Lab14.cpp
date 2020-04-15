cdad06365383deacd3788740d60b808ca0d501a0
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 19:46:00 +0500

    Add files via upload

---
+++ 15 Сортировка стека через stl-контейнеры/Lab14/Lab14.cpp
@@ -0,0 +1,61 @@
+﻿//Муллов Игорь, 15 Сортировка стека через stl-контейнеры
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    stack<int> first;
+    stack<int> second;
+    stack<int> third;
+
+
+    int k;
+    int n;
+    cout << "Введите длину стека:";
+    cin >> n;
+    cout << "Изначальный стек:";
+    for (int i = 0; i < n; i++)
+    {
+        first.push(k = rand() % 100 - 50);
+        cout << k << " ";
+    }
+    cout << endl;
+    int min, tmp;
+    while (!first.empty())
+    {
+        min = first.top();
+        first.pop();
+        second.push(min);
+        while (!first.empty())
+        {
+            tmp = first.top();
+            first.pop();
+            if (tmp > min)
+            {
+                min = tmp;
+            }
+            second.push(tmp);
+        }
+        while (!second.empty())
+        {
+            tmp = second.top();
+            second.pop();
+            if (tmp == min)
+            {
+                third.push(tmp);
+            }
+            else first.push(tmp);
+        }
+    }
+    cout << "Стек после сортировки: ";
+    while (!third.empty())
+    {
+        cout << third.top() << " ";
+        third.pop();
+    }
+
+    return 0;
+}
