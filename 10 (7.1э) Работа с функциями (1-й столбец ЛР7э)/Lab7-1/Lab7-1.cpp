57a8ef8d7cd379352681abbf73ca1701d0ef17ca
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 20:05:44 +0500

    Add files via upload

---
+++ 10 (7.1э) Работа с функциями (1-й столбец ЛР7э)/Lab7-1/Lab7-1.cpp
@@ -0,0 +1,38 @@
+﻿// Муллов Игорь, 10 (7.1э) Работа с функциями (1й столбец ЛР7э)
+
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+using namespace std;
+
+void Printf(const char name[]="Игорь",const char surname[]="Муллов",const char middlename[]="Евгеньевич")
+{
+    cout << surname <<'\t';
+    cout << name <<'\t';
+    cout << middlename << '\t' << endl;;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int n = 20;
+    char Name[n];
+    char Surname[n];
+    char Middlename[n];
+    cout << "Введите имя:" << endl;
+    cin.getline(Name,20);
+    cout << "Введите фамилию:" << endl;
+    cin.getline(Surname, 20);
+    cout << "Введите отчество:" << endl;
+    cin.getline(Middlename, 20);
+    cout << endl;
+   
+    Printf();
+    Printf(Name);
+    Printf(Name, Surname);
+    Printf(Name, Surname, Middlename);
+           
+}
+
---
+++ 10 (7.2э) Работа с функциями (2-й столбец ЛР7э)/Lab7-2/Lab7-2.cpp
@@ -0,0 +1,41 @@
+﻿// Муллов Игорь, 10 (7.2э) Работа с функциями (2-й столбец ЛР7э)
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void Print(int n, int first, ...)
+{    
+    int* p = &first;
+    while (n--)
+    {
+        cout << *p << " ";
+        p++;
+    }
+    cout << endl;
+}
+
+void Max(int n,int first, ...)
+{
+    int max = 0;
+    int* p = &first;
+    while(n--)
+    {
+        if (max < *p)
+        {
+            max = *p;          
+        }
+        p++;
+    }
+    cout << max << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Маскимальный элемент: 22,33,44,55:" << endl;
+    Max(4, 22, 33, 44, 55);
+    cout << "Маскимальный элемент: 6,54,-6372,28,322,1,4309:" << endl;
+    Max(6, 54, -6372, 28, 322, 1, 4309);    
+}
+
