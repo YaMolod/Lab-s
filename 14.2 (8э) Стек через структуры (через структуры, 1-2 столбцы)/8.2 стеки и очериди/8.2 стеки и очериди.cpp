e7d637b8a04e2b509961f001a4f8ae82286f2411
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 19:49:01 +0500

    Add files via upload

---
+++ 14.2 (8э) Стек через структуры (через структуры, 1-2 столбцы)/8.2 стеки и очериди/8.2 стеки и очериди.cpp
@@ -0,0 +1,103 @@
+﻿// Муллов Игорь, 14.2 (8э) Стек через структуры (через структуры, 1-2 столбцы)
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+    Node* next;
+    char* data;
+};
+
+Node* create_stack(Node*& top, char* data,int n)
+{
+    Node* r; 
+    for (int i = 1; i < n; i++)
+    {
+        r = new Node;
+        r->data = data;
+        r->next = top;
+        top = r;
+    }   
+    return top;
+}
+
+void print(Node* top)
+{
+    Node* p = top;   
+    while (p!=NULL)
+    {
+        cout << p->data << '\t';
+        p = p->next;
+    }
+    cout << endl;
+}
+
+Node* push(Node*& top,int k)
+{
+    Node* p = top;
+    Node* r = new Node;
+    cout << "Введите элемент стека:";
+    char s[50];
+    cin >> s;
+    char* data = new char[strlen(s) + 1];
+    strcpy(data, s);
+    r->data = data;
+    if (k == 1)
+    {
+        r->next = top;
+        top = r;
+        return top;
+    }
+    for (int i = 1; i < k - 1 && p->next != NULL; i++)
+    {
+        p = p->next;
+    }
+    if (p!= NULL)
+    {
+        r->next = p->next;
+        p->next = r;
+    }
+    return top;
+}
+
+Node* clear(Node*& top)
+{
+    while (top)
+    {
+        Node* p = top;
+        top = top->next;
+        delete p;
+        p = top;
+    }
+    top = NULL;
+    return top;
+}
+
+int main()
+{    
+    setlocale(LC_ALL, "ru");
+    int n ; int k;
+    cout << "Введеите количество элементов стека:";
+    cin >> n;
+    Node* top = new Node;
+    char s[50];
+    cout << "Введите элемент стека:";
+    cin >> s;
+    char* data = new char[strlen(s) + 1];
+    strcpy(data, s);
+    top->data = data;
+    top->next = NULL;
+    create_stack(top, data,n);
+    print(top);
+    cout << "Введите номер элемента:";
+    cin >> k;
+    push(top, k);
+    print(top);
+    clear(top);
+    delete[]data;
+}
+
+
---
+++ 14.2 (8э) очередь через структуры (через структуры, 1-2 столбцы)/8.2 очередь/8.2 очередь.cpp
@@ -0,0 +1,111 @@
+﻿// Муллов Игорь, 14.2 (8э)  очередь через структуры (через структуры, 1-2 столбцы)
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Queue
+{
+    Queue* next;
+    char* data;
+};
+
+Queue* make_queue(Queue*& first,Queue*& last, char* data, int n)
+{
+    first = new Queue;
+    first->data = data;
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        Queue* p = new Queue;
+        p->data = data;
+        p->next = last;
+        last = p;       
+    }
+    return last;
+}
+
+void Print(Queue* last)
+{
+    Queue* p = last;
+    while (p)
+    {
+        cout << p->data << '\t';
+        p = p->next;
+    }
+    cout << endl;
+}
+
+Queue* push(Queue*&first, Queue*& last, int k,int& n)
+{
+    Queue* p = last;
+    Queue* r = new Queue;
+    cout << "Введите элемент очереди:";
+    char s[50];
+    cin >> s;
+    char* data = new char[strlen(s) + 1];
+    strcpy(data, s);
+    r->data = data;
+    n++;
+    if (k == 1)
+    {
+        r->next = NULL;
+        first->next = r;
+        return first;
+    }
+    else if (k==n)
+    {
+        r->next = last;
+        last = r;
+        return last;
+    }
+    for (int i = 1; i < n-k  && p->next != NULL; i++)
+    {      
+        p = p->next;
+    }
+    if (p != NULL)
+    {
+        r->next = p->next;
+        p->next = r;
+        return last;
+    }    
+}
+
+Queue* clear(Queue*& first, Queue*& last)
+{  
+    while (last)
+    {
+        Queue* p = last;
+        last = last->next;
+        delete p;
+        p = last;
+    }
+    last = NULL;
+    first = NULL;
+    return last;   
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Queue* first = NULL; Queue* last=NULL;
+    int n=4; int k;
+    cout << "Введеите количество элементов очереди:";
+    cin >> n;
+    char s[50];
+    cout << "Введите элемент очереди:";
+    cin >> s;
+    char* data = new char[strlen(s) + 1];
+    strcpy(data, s);
+    make_queue(first, last, data, n);
+    Print(last);  
+    cout << "Введите номер элемента:";
+    cin >> k;
+    push(first, last, k,n);
+    Print(last);
+    clear(first, last);
+    delete[]data;
+}
+
