ddf1ffdea6920bb99a236a4c65e2f17f6319a1c8
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 20:23:29 +0500

    Add files via upload

---
+++ 6 (5-1э) Динамические массивы/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,105 @@
+﻿// Муллов Игорь, 6 (5-1э) Динамические массивы
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int* A;
+int n;
+
+
+int* CreateArray(int size)
+{
+    int* A = new int[size];
+    return A;
+}
+
+void FillArray(int* pA, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        pA[i] = rand() % 100;
+    }
+}
+
+void PrintArray(int* pA, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << pA[i] << " ";
+    }
+    cout << endl;
+}
+
+void push_back(int*& pA, int& n, int tmp)
+{
+    int* newpA = new int[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        newpA[i] = pA[i];
+    }
+    newpA[n] = tmp;
+    n++;
+    delete[]pA;
+    pA = newpA;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int tmp;
+    
+    int selection = 0;
+    
+    do
+    { 
+         cout << "Выберите действие" << endl <<
+        "1.Формирование массива" << endl <<
+        "2.Печать массива" << endl <<
+        "3.Добавление элемента в массив" << endl <<
+        "4.Выход" << endl;
+         
+        cin>>selection;
+        switch (selection)
+        {
+        case 1:
+            cout << "Введите длину массива:";
+            cin >> n;  
+            A = CreateArray(n);
+            FillArray(A, n);            
+            break;
+        case 2:
+            
+            PrintArray(A, n);
+            break;
+        case 3:
+            
+            cout << "Введите элемент:";
+            cin >> tmp;
+            push_back(A, n, tmp);
+            break;
+        case 4:
+            cout << "Выход из меню" << endl;
+            break;
+        default:
+            cout << "Пункт в меню отсутствует" << endl;
+            break;
+        }
+
+    }
+    while (selection!= 4);
+    return selection;
+
+    delete[] A;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ 6 (5-2э) Динамические массивы/Lab5-2/Lab5-2.cpp
@@ -0,0 +1,137 @@
+﻿// Муллов Игорь, 6 (5-2э) Динамические массивы
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int** A;
+int m, n, k;
+
+int** CreateArray (int rows, int cols)
+{
+	int** A = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		A[i] = new int[cols];
+	}
+	return A;
+}
+
+void FillArray(int** pA, int m, int n)
+{
+	/*for (int i = 0; i < m; i++)
+	{
+		pA[i] = new int[n];
+	}*/
+	
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			pA[i][j] = rand() % 101 - 50;			
+		}		
+	}
+}
+
+void PrintArray(int** pA, int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << pA[i][j] << '\t';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void push_back(int**& pA, int m, int &n,int k)
+{
+	n--;
+	int** newpA = new int* [m];
+	for (int i = 0; i < m; i++)
+	{
+		newpA[i] = new int[n];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j <=k; j++)
+		{
+			newpA[i][j] = pA[i][j];
+		}
+		for (int j = k-1; j < n; j++)
+		{
+			newpA[i][j] = pA[i][j + 1];
+		}	
+	}
+	
+	for (int i = 0; i < m; i++)
+	{
+		delete[] pA[i];
+	}
+	delete[]pA;
+	pA = newpA;
+}
+
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "ru");
+	int selection = 0;
+	
+	do
+	{
+		cout << "Выберите действие" << endl <<
+			"1.Формирование массива" << endl <<
+			"2.Печать массива" << endl <<
+			"3.Удаление столбца массива" << endl <<
+			"4.Выход" << endl;
+
+		cin >> selection;
+		switch (selection)
+		{
+		case 1:
+			cout << "Введите количество строк массива:";
+			cin >> m;
+			cout << "Введите количество столбцов массива:";
+			cin >> n;
+			A = CreateArray(m, n);
+			FillArray(A, m, n);
+			break;
+		case 2:
+
+			PrintArray(A, m, n);
+			break;
+		case 3:
+
+			cout << "Введите номер столбца:";
+			cin >> k;
+			while (k<1 || k>n)
+			{
+				cout << "Ошибка! Ведите другой номер" << endl;
+				cin >> k;
+			}
+			push_back(A, m, n, k);
+			break;
+		case 4:
+			cout << "Выход из меню" << endl;
+			break;
+		default:
+			cout << "Пункт в меню отсутствует" << endl;
+			break;
+		}
+
+	} while (selection != 4);
+	return selection;
+	
+
+
+	for (int i = 0; i < m; i++)
+	{
+		delete [] A[i];
+	}
+	delete[] A;		
+}
