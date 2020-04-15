0cfc0836a034076907e0d9f7557e9dc4bf3dbffe
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 19:52:51 +0500

    Add files via upload

---
+++ 14.1 (8э) Однонаправленные списки (через структуры, 1-2 столбцы)/Lab8.1/Lab8.1.cpp
@@ -0,0 +1,111 @@
+﻿//Муллов Игорь, 14.1 (8э) Однонаправленные списки (через структуры, 1-2 столбцы)
+#include <iostream>
+#include <stdlib.h> 
+#include <ctime>
+
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+Node* make_list(Node*& first, int n)
+{
+    Node* p; Node* r;
+    p = new Node;
+    p->data = rand() % 100 - 50;
+    p->next = NULL;
+    first = p;
+    for (int i = 1; i < n; i++)
+    {
+        r = new Node;
+        r->data = rand() % 100 - 50;
+        r->next = NULL;
+        p->next = r;
+        p = r;
+    }
+    return first;
+}
+void Print(Node* first)
+{
+    while (first)
+    {
+        cout << first->data << '\t';
+        first = first->next;
+    }
+    cout << endl;
+}
+
+Node* del(Node*& first,int n)
+{
+    Node* tmp = nullptr;
+    Node* ptr = nullptr;
+    Node* p = first;
+    for (int i = 1; i < n - 1 && p->next != NULL; i++)
+    {        
+        
+
+        p = p->next;
+       
+       
+        if (p->next->data % 2 == 0 && p->next != NULL)
+        {
+            ptr = p;
+            tmp = p->next;
+        }  
+    }
+    if (tmp == nullptr && first->data%2!=0)
+    {
+        return first;
+    }  
+    else if (tmp!=nullptr)
+    {
+        ptr->next = tmp->next;
+        delete tmp;
+        return first;
+       
+    }
+    else 
+    {       
+        p = first;
+        first = first->next;
+        delete p;
+    }  
+    
+}
+
+Node* clear(Node* first)
+{
+    while (first)
+    {
+        Node* p = first;
+        first = first->next;
+        delete p;
+    }
+    first = NULL;
+    return first;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    Node* first = NULL;
+    int n;
+    cout << "Введите количество узлов:";
+    cin >> n;
+    int k = 0;
+    make_list(first, n);
+    Print(first);
+    
+    del(first,n);
+   
+    Print(first);
+    clear(first);
+}
+
+
---
+++ 14.1 (8э) двунаправленные списки (через структуры, 1-2 столбцы)/Lab8.2/Lab8.2.cpp
@@ -0,0 +1,103 @@
+﻿//Муллов Игорь,14.1 (8э)  двунаправленные списки(через структуры, 1 - 2 столбцы)
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+using namespace std;
+
+struct Node
+{
+    char* data;
+    Node* next;
+    Node* prev;
+};
+
+Node* make_list(Node*& first, int n)
+{
+    Node* p; Node* r;
+    p = new Node;
+    char s[50];
+    cout << "Введите элемент узла:" << endl;
+    cin >> s;
+    p->data = new char[strlen(s) + 1];
+    strcpy(p->data, s);
+    p->next = NULL;
+    p->prev = NULL;
+    first = p;
+    for (int i = 1; i < n; i++)
+    {
+        r = new Node;
+        r->prev = p;
+        r->next = NULL;
+        char s[50];
+        cout << "Введите элемент узла:" << endl;
+        cin >> s;
+        r->data = new char[strlen(s) + 1];
+        strcpy(r->data, s);
+        p->next = r;
+        p = r;
+    }
+    return first;
+}
+
+void Print(Node* first)
+{
+    cout << "Вывод списка:"<<endl;
+    while (first)
+    {
+        cout << first->data<<'\t';
+        first = first->next;
+    }
+}
+
+Node* add(Node*& first,int k)
+{
+    Node* p = first;
+    Node* r = new Node;
+    r->prev = NULL;
+    r->next = NULL;
+    char s[50];
+    cout << "Введите элемент узла:" << endl;
+    cin >> s;
+    r->data = new char[strlen(s) + 1];
+    strcpy(r->data, s);
+    if (k == 1)
+    {
+        r->next = first;
+        first->prev = r;
+        first = r;
+        r->prev = NULL;
+        return first;
+    }
+    for (int i = 1; i < k-1 && p->next != NULL; i++)
+    {
+        p = p->next;
+    }
+    if (p != NULL)
+    {
+        r->next = p->next;
+        p->next = r;
+        r->prev = p;        
+        return first;
+    } 
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Node* first = NULL;
+    cout << "Введите количество узлов списка:";
+    int n;
+    cin >> n;
+    int k;
+    make_list(first, n);
+    Print(first);
+    cout << endl << "Введите номер элемента:";
+    cin >> k;
+    add(first, k);
+    Print(first);
+}
+
