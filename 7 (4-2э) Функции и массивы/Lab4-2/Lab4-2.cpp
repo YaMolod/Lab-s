7d480220487c69960bf6e44439ba6d6ed4ed7d60
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-15 20:20:43 +0500

    Add files via upload

---
+++ 7 (4-2э) Функции и массивы/Lab4-2/Lab4-2.cpp
@@ -0,0 +1,65 @@
+﻿// Муллов Игорь, 7 (4-2э) Функции и массивы
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void FillArray (int (&arr)[100][100], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = rand() % 100 - 50;
+        }
+    }
+}
+
+void PrintArray (int(&arr)[100][100], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void TranspArray(int(&arr)[100][100], int m, int n)
+{
+    int tmp;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j % 2 == 0)
+            {
+                tmp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = tmp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int m, n;
+    cout << "Введите количество строк" << endl;
+    cin >> m;
+    cout << "Введите количество столбцов" << endl;
+    cin >> n;
+    int A[100][100];
+
+    FillArray(A, m, n);
+    PrintArray(A, m, n);
+
+    TranspArray(A, m, n);
+    PrintArray(A, m, n);
+}
+
---
+++ 7 (4-3э) Функции и массивы/Lab13/Lab13.cpp
@@ -0,0 +1,72 @@
+﻿// Муллов Игорь, 7 (4-3э) Функции и массивы
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+void sortstr(string& str)
+{	
+	int lenstr = str.length(), wcnt = 0, j = 0;
+	if (lenstr > 0)
+	{
+		
+		if (lenstr == 1 && str[0] != ' ')
+			wcnt = 1;
+		else
+		{
+			for (int i = 0; i < lenstr - 1; i++)
+			{
+				if ((str[i] == ' ' && str[i + 1] != ' ') || (i == 0 && str[i] != ' '))
+					wcnt++;
+			}
+		}
+				
+		string* warr = new string[wcnt];
+		
+		for (int i = 0; i < lenstr; i++)
+		{
+			
+			while (str[i] != ' ' && i < lenstr)
+			{
+				warr[j] += str[i];
+				i++;
+			}
+			
+			j++;
+
+		}
+		
+		for (int i = 0; i < wcnt - 1; i++)
+		{
+			for (int j = 0; j < wcnt - i - 1; j++)
+			{
+				if (warr[j][0] > warr[j + 1][0])
+				{
+					swap(warr[j], warr[j + 1]);
+				}
+			}
+		}
+		
+		str = warr[0] + ' ';
+		for (int i = 1; i < wcnt; i++)
+		{
+			str = str + warr[i] + ' ';
+		}
+		delete[] warr;
+	}
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string str;
+	cout << "Введите строку:" << endl;
+	getline(cin, str);
+	sortstr(str);
+	cout << "Отсортированная строка: "<<endl << str;
+}
+
