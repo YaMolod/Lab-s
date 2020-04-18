cf84790e4befe4a5bef9f62d4ccae4cc5dbd2296
YaMolod <63513056+YaMolod@users.noreply.github.com>
2020-04-18 19:50:59 +0500

    Add files via upload

---
+++ (13). Рекурсивные функции/Lab10/Lab10.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+void recurs(int n, int m, int &k)
+{
+    if (n != m)
+    {
+        if (n > m)
+        {
+            n = n - m;
+            k++;
+        }
+        if (m > n)
+        {
+            m = m - n;
+            k++;
+        }
+        recurs(n, m, k);
+    }    
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n, m, k = 0;
+    cout << "Введите длину стороны А: ";
+    cin >> n;
+    cout << "Введите длину стороны В: ";
+    cin >> m;
+    recurs(n, m, k);
+    {
+        cout << "Количество квадратов:  " << k+1 << endl;
+    }
+}
